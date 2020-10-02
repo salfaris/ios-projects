676058c1f6a31d136d09449bcb5086b7713b90bc
salfaris <sal.ahmadfaris@gmail.com>
2020-10-02 13:04:48 +0100

    Initial commit for GitHub followers. Finished intial setup.

---
+++ GithubFollowers/GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Salman Faris on 01/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
